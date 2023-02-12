fa1d4e0af5991fee8e5bca76b868b7459be9b414
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-02-12 09:43:46 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b**2-4*a*c);
+  if (d>0){
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+     arr.push(x1,x2);
+     console.log(arr);
+     return arr;
+  }
+  if (d===0){
+    let x0 = -b/(2*a);
+    arr.push(x0);
+    console.log(arr);
+  }
+  else{
+    console.log("Корень отсутствует " + arr)
+  }
   return arr;
 }
 
