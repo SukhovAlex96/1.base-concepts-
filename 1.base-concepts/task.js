2e662bcc850c6751e94122fae4e3ff0f50776846
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-02-12 10:02:09 +0300

    Второе задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,5 +21,30 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+ 
+  let per = parseInt(percent) / 100 / 12;
+  let con = parseInt(contribution); 
+  let am = parseInt(amount);
+
+    if (isNaN(per) || per < 0) {
+   		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+   	} else if (isNaN(con) || con < 0) {
+   		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+   	} else if (isNaN(am) || am < 0) {
+   		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+   	} else {
+			
+  let today = new Date();
+
+  	if (today.getFullYear() > date.getFullYear() || today.getMonth() > date.getMonth()) {
+			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
+		} else{
+        let s = am - con;
+        let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear()));
+        let pay = s*(per+per/(((1+per)**n)-1));
+        let totalAmount = (pay * n).toFixed(2);
+        console.log(totalAmount);
+        return + totalAmount;
+    }
+  }
 }
