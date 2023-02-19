679bc5dc8a7038398d5a8813f73f98e7f5e21255
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-02-19 17:20:45 +0300

    Первое задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,21 @@
-function getArrayParams(...arr) {
+"use strict"
 
+function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+
+  for (let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+    if (arr[i > max]){
+      max = arr[i];
+    }
+    if (arr[i] < min){
+      min =arr[i];
+    }
+  }
+  avg = perseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
