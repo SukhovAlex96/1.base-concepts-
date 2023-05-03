0e4ef9d6cc586983352be96eb1ca43d158d7f68d
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-05-03 20:28:05 +0300

    работа над ошибками

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,25 +1,20 @@
 function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
-    this.age = age; 
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (mark){
-    (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
-}
-
-Student.prototype.addMarks = function (...marks){
-  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+Student.prototype.addMarks = function (...marks) {
+  return this.marks ? this.marks.push(...marks) : "Отчислен";
 }
 
 Student.prototype.getAverage = function () {
-  let sum = 0;
-  this.marks.forEach(item => sum += item);
-  return sum / this.marks.length;
+  return this.marks && this.marks.length ? this.marks.reduce((acc, cur) => acc + cur, 0) / this.marks.length : 0;
 }
 
 Student.prototype.exclude = function (reason) {
